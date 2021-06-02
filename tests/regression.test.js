09512e3ecea774c6523d3ae53a06284a7a0476d1
Tomek Łaziuk <tlaziuk@gmail.com>
2021-06-02 17:05:55 +0200

    set up regression tests

---
+++ tests/regression.test.js
@@ -0,0 +1,17 @@
+describe('chart regression', () => {
+  it.each([
+    ['one_line.json']
+    // ['two_lines.json'], // disabled,
+    // currently this library does not support creating multiple Chart instances in a stable manner,
+    // any following chart instance will have some kind of artifacts
+  ])('should match the snapshoot for %p', async (filename) => {
+    const Chart = require('../src/Chart')
+    const chart = new Chart(require(`./fixtures/payloads/${filename}`))
+    const chartBuffer = await chart.get()
+
+    expect(chartBuffer).toMatchImageSnapshot({
+      failureThreshold: 0.01,
+      failureThresholdType: 'percent'
+    })
+  })
+})
