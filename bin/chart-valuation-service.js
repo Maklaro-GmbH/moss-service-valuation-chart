d57443ef5bac9e40fd3ffe4dc7a26ca51d208e69
Tomek Łaziuk <tlaziuk@gmail.com>
2021-06-14 14:44:43 +0200

    Fix usage as an external package (#11)

--- bin/chart-valuation-service.js
+++ bin/chart-valuation-service.js
@@ -1,6 +1,7 @@
 #!/usr/bin/env node
 
 require('ts-node').register({
+  project: require('path').resolve(__dirname, '..', 'tsconfig.json'), // setting the `project` allows to use this package as a typescript "binary"
   transpileOnly: true
 })
 
